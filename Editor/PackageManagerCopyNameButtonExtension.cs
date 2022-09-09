cf019d7c2f98ff26c50168f4a0a92fc9a46a6872
baba-s <shotababa1206@gmail.com>
2022-09-09 19:59:49 +0900

    add

---
+++ Editor/PackageManagerCopyNameButtonExtension.cs
@@ -0,0 +1,76 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal sealed class PackageManagerCopyNameButtonExtension
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private bool        m_isInitialized;
+        private PackageInfo m_selectedPackageInfo;
+
+        static PackageManagerCopyNameButtonExtension()
+        {
+            var extension = new PackageManagerCopyNameButtonExtension();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_isInitialized = false;
+            return this;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            Initialize();
+
+            m_selectedPackageInfo = packageInfo;
+        }
+
+        private void Initialize()
+        {
+            if ( m_isInitialized ) return;
+
+            VisualElement root = this;
+
+            while ( root is { parent: { } } )
+            {
+                root = root.parent;
+            }
+
+            var copyNameButton = new Button
+            (
+                () =>
+                {
+                    var packageName = m_selectedPackageInfo.name;
+
+                    EditorGUIUtility.systemCopyBuffer = packageName;
+
+                    Debug.Log( $"Copied! `{packageName}`" );
+                }
+            )
+            {
+                text = "Copy Name",
+            };
+
+            var removeButton = root.FindElement( x => x.name == "PackageRemoveCustomButton" );
+            removeButton.parent.Insert( 0, copyNameButton );
+
+            m_isInitialized = true;
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.PackageManagerCopyNameButtonExtension
+# Kogane Package Manager Copy Name Button Extension
+
+パッケージ名をコピーするボタンを Package Manager に追加するエディタ拡張
